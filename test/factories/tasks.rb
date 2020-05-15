d64a05bfd9733c34daad2e6536a7e2a4bed630c0
ilya <sad1lovbatya@gmail.com>
2020-05-15 20:12:04 +0400

    factory and test model

---
+++ test/factories/tasks.rb
@@ -0,0 +1,14 @@
+
+FactoryBot.define do
+  factory :task do
+    name
+    description
+
+    trait :have_author do
+      author factory: :manager
+    end
+    trait :have_assignee do
+      assignee factory: :developer
+    end
+  end
+end
