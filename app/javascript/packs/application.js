4992dc2e83f89c093ca4883cab816a0f1f3880ce
ilya <sad1lovbatya@gmail.com>
2020-05-19 16:23:24 +0400

    eslint prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,14 +4,15 @@
 // that code so it'll be compiled.
 
 import 'material-design-lite/material.js';
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
 
-import WebpackerReact from 'webpacker-react'
-import TaskBoard from './components/TaskBoard'
+require('@rails/ujs').start();
+require('@rails/activestorage').start();
+require('channels');
 
-WebpackerReact.setup({ TaskBoard })
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from './components/TaskBoard';
+
+WebpackerReact.setup({ TaskBoard });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
