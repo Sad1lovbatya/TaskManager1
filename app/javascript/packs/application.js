5618a30aeaea4fe507013f595c97bbe01fffd69e
ilya <sad1lovbatya@gmail.com>
2020-05-28 09:42:41 +0400

    add Task and show board

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require('@rails/activestorage').start();
 require('channels');
 
 import WebpackerReact from 'webpacker-react';
-import TaskBoard from './components/TaskBoard';
+import TaskBoard from 'components/TaskBoard';
 
 WebpackerReact.setup({ TaskBoard });
 // Uncomment to copy all static images under ../images to the output folder and reference
