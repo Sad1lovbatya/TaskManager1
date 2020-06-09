6f391cad6c1e9ec7c090b0c5874bd58a18a4e92d
ilya <sad1lovbatya@gmail.com>
2020-06-09 10:31:40 +0400

    fix

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -3,6 +3,5 @@ require 'test_helper'
 class Web::BoardsControllerTest < ActionController::TestCase
   test 'should get new' do
     get :show
-    assert_response :success
-  end
+    assert_response :redirect
 end
